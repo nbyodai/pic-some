71d3b2599c340d7ab39a5a9969fa6c4821243268
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-28 22:26:06 +0100

    Fetch data

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Context = React.createContext();
-
 function ContextProvider({ children }) {
   const [allPhotos, setAllPhotos] = useState([]);
+  const url =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(() => {
+    fetch(url, {})
+      .then((response) => response.json())
+      .then((data) => {
+        setAllPhotos(data);
+      });
+  }, []);
 
   return (
     <Context.Provider value={{ allPhotos, setAllPhotos }}>
