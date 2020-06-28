16a17fea03176c7eb90d58fb4974590fb74bbd4f
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-28 18:30:56 +0100

    Set up project environment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default App;
