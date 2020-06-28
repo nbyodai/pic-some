d75e4bc253d03f4c1035cbaeaeca308c63085d77
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-28 23:52:21 +0100

    Add Proptypes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,15 @@
 import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+
 import { Context } from "../Context";
 
 function Image({ className, img }) {
   const [hovered, setHovered] = useState(false);
   const { toggleFavorite } = useContext(Context);
-  const heartIcon = hovered && (
+  const heartIcon = (hovered || img.isFavorite) && (
     <i
       onClick={() => toggleFavorite(img.id)}
-      className="ri-heart-line favorite"
+      className={`ri-heart-${img.isFavorite ? "fill" : "line"} favorite`}
     ></i>
   );
   const plusIcon = hovered && <i className="ri-add-circle-line cart"></i>;
@@ -23,5 +25,14 @@ function Image({ className, img }) {
     </div>
   );
 }
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool,
+  }).isRequired,
+};
 
 export default Image;
