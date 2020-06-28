73df7dd6e8bd723ccdb618689f79520c3154016d
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-28 23:14:49 +0100

    Add mouse events to images

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Image({ className, img }) {
+  const [hovered, setHovered] = useState(false);
+
+  const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+  const plusIcon = hovered && <i className="ri-add-circle-line cart"></i>;
   return (
     <div className={`${className} image-container`}>
-      <img src={img.url} className="image-grid" alt="img-prop" />
+      <img
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        src={img.url}
+        className="image-grid"
+        alt="img-prop"
+      />
+      {heartIcon}
+      {plusIcon}
     </div>
   );
 }
