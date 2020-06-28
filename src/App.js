1483c12ed7c61196a478e284b181b22057aabda9
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-28 18:47:40 +0100

    Add router to switch pages on page links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header";
+import Photos from "./pages/Photos";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
   );
 }
