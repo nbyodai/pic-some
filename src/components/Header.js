11720a80983ffc524503d0dc9f85572e829fcd67
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-29 01:08:25 +0100

    Modify header cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { Context } from "../Context";
+
 function Header() {
+  const { cartItems } = useContext(Context);
+  const cartIconFill = cartItems.length ? "fill" : "line";
+
   return (
     <header>
       <h2>
         <Link to="/">Pic Some</Link>
       </h2>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        <i className={`ri-shopping-cart-${cartIconFill} ri-fw ri-2x`}></i>
       </Link>
     </header>
   );
