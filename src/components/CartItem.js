27f72c321e5346430aac6a855c55e25074b5c256
Chibuzor Obiora <nbyodai@gmail.com>
2020-06-29 09:38:58 +0100

    Add hover effect and proptypes to cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,19 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import PropTypes from "prop-types";
 import { Context } from "../Context";
 
 function CartItem({ item }) {
   const { removeCartItem } = useContext(Context);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <div className="cart-item">
       <i
         onClick={() => removeCartItem(item.id)}
-        className="ri-delete-bin-line"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className={`ri-delete-bin-${hovered ? "fill" : "line"}`}
       ></i>
       <img src={item.url} width="130px" alt={"cart item"} />
       <p>$5.99</p>
     </div>
   );
 }
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }),
+};
 
 export default CartItem;
